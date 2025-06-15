ec2d4e335cab9c7d9bf13741e7795a06e415ff3e
GabrielUFSM <gbmoro@inf.ufsm.br>
2025-06-15 15:38:48 -0300

    feat: tela de login

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/project3-2025a-gabriel/',
+  base: '/project4-2025a-gabriel-1/',
   build: {
     outDir: '../docs'
 	}
