a0430c4e99a567548d9b9ee56d92cdb6cf628f2a
GabrielUFSM <gbmoro@inf.ufsm.br>
2025-06-23 17:59:38 -0300

    fix: cor de fonte correta no menu-footer input

--- frontend/src/components/WhiteboardMenu.vue
+++ frontend/src/components/WhiteboardMenu.vue
@@ -392,6 +392,7 @@ watch(userInfo, (newUserInfo) => {
   border: 1px solid #dadce0;
   border-radius: 8px;
   background-color: #fff;
+  color: #202124;
 }
 .menu-footer input:focus {
   outline: none;
